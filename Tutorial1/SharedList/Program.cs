ea9da2e0a503255133667cb6ccbdbc3469dc69f0
Скорлыгин Никита Сергеевич <nikita.skorlygin@alt.altarix.ru>
2017-06-08 17:31:50 +0400

    говнокод на тему потоков

---
+++ Tutorial1/SharedList/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace SharedList
+{
+	/// <summary>
+	/// В сборочном цеху завода, вокруг одного стола работает несколько роботов
+	/// Один робот раскладывает по столу заготовки, еще несколько роботов собирают их.
+	/// Роботы-сборщики берут деталь со стола и собирают ее в два этапа, сборка деталей моментальная, но после сборки роботу необходимо немного остыть.
+	/// За отведенный период времени заготовки должны быть собраны. 
+	/// 
+	/// Доработать код так, чтобы бракованных изделий не было, а количество недоделанных свести к минимуму
+	/// Как можно улучшить этот код? Какие проблемы вы заметили? Улучшайте!
+	/// </summary>
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			FrameProvider frameProvider = new FrameProvider();
+
+			Assembler assembler = new Assembler();
+
+			Thread provider = new Thread(frameProvider.Start);
+			provider.Start();
+
+			for (int i = 0; i < 5; i++)
+			{
+				new Thread(assembler.Start).Start();
+			}
+
+			Thread.Sleep(10000);
+
+			ListManager.StopTrigger = true;
+			Console.WriteLine(ListManager.GetInfo());
+			Console.ReadLine();
+			Console.ReadLine();
+		}
+
+		//Рама = 0,
+		//Полусобрано = 1,
+		//Собрано = 2,
+		//Испорчено >= 3
+
+		public static class ListManager
+		{
+			public static bool StopTrigger = false;
+			public static List<int> SharedList;
+			static ListManager()
+			{
+				SharedList = new List<int>();
+			}
+
+			public static string GetInfo()
+			{
+				string result = "";
+				int frames = 0;
+				int halfs = 0;
+				int assembled = 0;
+				int broken = 0;
+
+				foreach (var element in SharedList)
+				{
+					switch (element)
+					{
+						case 0:
+							{
+								frames++;
+								break;
+							}
+
+						case 1:
+							{
+								halfs++;
+								break;
+							}
+
+						case 2:
+							{
+								assembled++;
+								break;
+							}
+						default:
+							{
+								broken++;
+								break;
+							}
+					}
+					result = String.Format("Итого:\r\n" +
+										   "Рамы:{0}\r\n" +
+										   "Полусобрано:{1}\r\n" +
+										   "Собрано:{2}\r\n" +
+										   "Испорчено:{3}\r\n",
+						frames, halfs, assembled, broken
+					);
+				}
+
+				return result;
+			}
+
+			public static int GetIndex(int[] requiredConditions)
+			{
+				for (int i = 0; i < SharedList.Count; i++)
+				{
+					if (requiredConditions.Contains(SharedList[i]))
+					{
+						return i;
+					}
+				}
+				return -1;
+			}
+		}
+
+		public class FrameProvider : Robot
+		{
+			//Добавляет продукты в общий список
+			public override void DoWork()
+			{
+				if (ListManager.SharedList.Count < 150)
+					ListManager.SharedList.Add(0);
+			}
+		}
+
+		//Сборщики должны брать в работу только пустые рамы или недоделанные изделия
+		public class Assembler : Robot
+		{
+			public override void DoWork()
+			{
+				int index = ListManager.GetIndex(new[] { 0, 1 });
+
+				if (index >= 0)
+				{
+					ListManager.SharedList[index]++;
+					Thread.Sleep(250);
+				}
+			}
+		}
+
+		public abstract class Robot
+		{
+			public void Start()
+			{
+				while (!ListManager.StopTrigger)
+				{
+					DoWork();
+				}
+			}
+
+			public abstract void DoWork();
+		}
+	}
+}
---
+++ Tutorial1/Tutorial1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace Tutorial1
+{
+	class Program
+	{
+		/// <summary>
+		/// Переделать код так, чтобы потоки выполнялись один за другим и на момент вывода на экран состояние было "Finished"
+		/// Как можно улучшить этот код? Какие проблемы вы заметили? Улучшайте!
+		/// </summary>
+		/// <param name="args"></param>
+		static void Main(string[] args)
+		{
+			Worker worker = new Worker();
+
+			while (true)
+			{
+				Console.ReadLine();
+				Thread worker1 = new Thread(worker.Start);
+				Thread worker2 = new Thread(worker.Finish);
+
+				worker1.Start();
+				worker2.Start();
+
+				Console.WriteLine(worker.Condition);
+			}
+		}
+
+		public class Worker
+		{
+			public string Condition = "none";
+
+			public void Start()
+			{
+				Condition = "Just started";
+			}
+
+			public void Finish()
+			{
+				Condition = "Finished";
+			}
+		}
+	}
+}
