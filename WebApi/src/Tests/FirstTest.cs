625904d27e31b9f216d2092b65c39ff1ef246781
Скорлыгин Никита Сергеевич <nikita.skorlygin@alt.altarix.ru>
2017-07-04 18:54:45 +0400

    проект с юнит-тестами

---
+++ WebApi/src/Tests/FirstTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+	public class FirstTest
+	{
+		[Test]
+		public void PassingTest()
+		{
+			Assert.AreEqual(2, 1 << 1);
+		}
+
+		[Test]
+		public void FailingTest()
+		{
+			Assert.AreEqual(2, 1 >> 1);
+		}
+	}
+}
