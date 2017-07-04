c8821d173340ca08a7ba774bb6361a1640919864
Скорлыгин Никита Сергеевич <nikita.skorlygin@alt.altarix.ru>
2017-07-04 19:01:42 +0400

    мокаем контекст

--- WebApi/src/Tests/FirstTest.cs
+++ WebApi/src/Tests/FirstTest.cs
@@ -1,19 +1,50 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using WebApi.Models;
 
 namespace Tests
 {
 	public class FirstTest
 	{
 		[Test]
-		public void PassingTest()
-		{
-			Assert.AreEqual(2, 1 << 1);
-		}
+		public void PassingTest() { Assert.AreEqual(2, 1 << 1); }
 
 		[Test]
-		public void FailingTest()
+		public void FailingTest() { Assert.AreEqual(2, 1 >> 1); }
+
+		[Test]
+		public void DbContext()
 		{
-			Assert.AreEqual(2, 1 >> 1);
+			var data = new List<Message>
+			{
+				new Message
+				{
+					Id = Guid.NewGuid(),
+					CreateDate = DateTimeOffset.UtcNow,
+					Text = Guid.NewGuid().ToString()
+				},
+				new Message
+				{
+					Id = Guid.NewGuid(),
+					CreateDate = DateTimeOffset.UtcNow,
+					Text = Guid.NewGuid().ToString()
+				}
+			}.AsQueryable();
+
+			var mockSet = new Mock<DbSet<Message>>();
+			mockSet.As<IQueryable<Message>>().Setup(__messages => __messages.Provider).Returns(data.Provider);
+			mockSet.As<IQueryable<Message>>().Setup(__messages => __messages.Expression).Returns(data.Expression);
+			mockSet.As<IQueryable<Message>>().Setup(__messages => __messages.ElementType).Returns(data.ElementType);
+			mockSet.As<IQueryable<Message>>().Setup(__messages => __messages.GetEnumerator()).Returns(() => data.GetEnumerator());
+
+			var mockContext = new Mock<ApiDbContext>();
+			mockContext.Setup(c => c.Messages).Returns(mockSet.Object);
+
+			Assert.AreEqual(2, mockContext.Object.Messages.Count());
 		}
 	}
 }
